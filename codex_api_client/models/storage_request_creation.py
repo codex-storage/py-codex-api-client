a524ccf5ffe6fd62024d0caa8b472140cba0a081
AuHau <6072250+AuHau@users.noreply.github.com>
2025-03-31 16:47:37 +0000

    Generated API update

--- codex_api_client/models/storage_request_creation.py
+++ codex_api_client/models/storage_request_creation.py
@@ -19,6 +19,7 @@ import json
 
 from pydantic import BaseModel, ConfigDict, Field, StrictInt, StrictStr
 from typing import Any, ClassVar, Dict, List, Optional
+from typing_extensions import Annotated
 from typing import Optional, Set
 from typing_extensions import Self
 
@@ -29,8 +30,8 @@ class StorageRequestCreation(BaseModel):
     duration: StrictInt = Field(description="The duration of the request in seconds")
     price_per_byte_per_second: StrictStr = Field(description="The amount of tokens paid per byte per second per slot to hosts the client is willing to pay", alias="pricePerBytePerSecond")
     proof_probability: StrictStr = Field(description="How often storage proofs are required as decimal string", alias="proofProbability")
-    nodes: Optional[StrictInt] = Field(default=3, description="Minimal number of nodes the content should be stored on")
-    tolerance: Optional[StrictInt] = Field(default=1, description="Additional number of nodes on top of the `nodes` property that can be lost before pronouncing the content lost")
+    nodes: Optional[Annotated[int, Field(strict=True, ge=3)]] = Field(default=3, description="Minimal number of nodes the content should be stored on")
+    tolerance: Optional[Annotated[int, Field(strict=True, ge=1)]] = Field(default=1, description="Additional number of nodes on top of the `nodes` property that can be lost before pronouncing the content lost")
     collateral_per_byte: StrictStr = Field(description="Number as decimal string that represents how much collateral per byte is asked from hosts that wants to fill a slots", alias="collateralPerByte")
     expiry: StrictInt = Field(description="Number that represents expiry threshold in seconds from when the Request is submitted. When the threshold is reached and the Request does not find requested amount of nodes to host the data, the Request is voided. The number of seconds can not be higher then the Request's duration itself.")
     __properties: ClassVar[List[str]] = ["duration", "pricePerBytePerSecond", "proofProbability", "nodes", "tolerance", "collateralPerByte", "expiry"]
